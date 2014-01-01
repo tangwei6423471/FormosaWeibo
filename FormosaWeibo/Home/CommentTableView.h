c52c1637b277ec6c1a3ff2ec11c820ad79d3493f
Xincode9 <Xincode9@gmail.com>
2014-01-01 09:42:24 +0800

    1.評論列表加上 下拉加載 , 上拉加載更多 2.微博正文中顯示微博大圖

---
+++ FormosaWeibo/Home/CommentTableView.h
@@ -0,0 +1,13 @@
+//
+//  CommentTableView.h
+//  FormosaWeibo
+//
+//  Created by Joey on 2013/12/30.
+//  Copyright (c) 2013年 Joey. All rights reserved.
+//
+
+#import "BaseTableView.h"
+
+@interface CommentTableView : BaseTableView
+
+@end
---
+++ FormosaWeibo/Home/CommentTableView.m
@@ -0,0 +1,31 @@
+//
+//  CommentTableView.m
+//  FormosaWeibo
+//
+//  Created by Joey on 2013/12/30.
+//  Copyright (c) 2013年 Joey. All rights reserved.
+//
+
+#import "CommentTableView.h"
+
+@implementation CommentTableView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
