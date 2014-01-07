8478202efc692f7156fc792d7509948860fbcf13
Xincode9 <Xincode9@gmail.com>
2014-01-07 21:06:31 +0800

    新增微博視圖可大小圖切換瀏覽的功能

---
+++ FormosaWeibo/More/BrowModeViewController.h
@@ -0,0 +1,13 @@
+//
+//  BrowModeViewController.h
+//  FormosaWeibo
+//
+//  Created by Joey on 2014/1/7.
+//  Copyright (c) 2014年 Joey. All rights reserved.
+//
+
+#import "BaseViewController.h"
+
+@interface BrowModeViewController : BaseViewController
+
+@end
---
+++ FormosaWeibo/More/BrowModeViewController.m
@@ -0,0 +1,38 @@
+//
+//  BrowModeViewController.m
+//  FormosaWeibo
+//
+//  Created by Joey on 2014/1/7.
+//  Copyright (c) 2014年 Joey. All rights reserved.
+//
+
+#import "BrowModeViewController.h"
+
+@interface BrowModeViewController ()
+
+@end
+
+@implementation BrowModeViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
